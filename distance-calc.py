b8fab03b94bf723aef3797e39413c223378911e1
JungleHornet <86211409+JungleHornet@users.noreply.github.com>
2023-12-14 11:36:38 -0600

    Fixed simplifying radicals and added looping

--- distance-calc.py
+++ distance-calc.py
@@ -3,64 +3,78 @@ import math
 import sys    
 
 print("Welcome to JungleHornet's 2D distance calculator")
-print("Please input point 1 (x,y)")
-inpt1 = input()
-coord1 = re.split("[(]\s*(\d+)\s*[,]\s*(\d+)\s*[)]", inpt1)
-x1 = coord1[1]
-y1 = coord1[2]
 
-x1 = int(x1)
-y1 = int(y1)
+def main():
+    print("Please input point 1 (x,y)")
+    inpt1 = input()
+    coord1 = re.split("[(]\s*(\d+)\s*[,]\s*(\d+)\s*[)]", inpt1)
+    x1 = coord1[1]
+    y1 = coord1[2]
 
-print("Please input point 2 (x,y)")
-inpt2 = input()
-coord2 = re.split("[(]\s*(\d+)\s*[,]\s*(\d+)\s*[)]", inpt2)
-x2 = coord2[1]
-y2 = coord2[2]
+    x1 = int(x1)
+    y1 = int(y1)
 
-x2 = int(x2)
-y2 = int(y2)
+    print("Please input point 2 (x,y)")
+    inpt2 = input()
+    coord2 = re.split("[(]\s*(\d+)\s*[,]\s*(\d+)\s*[)]", inpt2)
+    x2 = coord2[1]
+    y2 = coord2[2]
 
-a = x2 - x1
-b = y2 - y1
+    x2 = int(x2)
+    y2 = int(y2)
 
-a = a**2
-b = b**2
+    a = x2 - x1
+    b = y2 - y1
+
+    a = a**2
+    b = b**2
 
 
-dist = math.sqrt(a + b)
+    dist = math.sqrt(a + b)
 
-rounded = round(dist)
-if rounded < dist:
-    rounded = rounded + 1
+    rounded = round(dist)
+    if rounded < dist:
+        rounded = rounded + 1
 
-root = 0
-simpleRoot = 0
-rootCoefficient = 1
-sqrtDist = 0
+    root = 0
+    simpleRoot = 0
+    rootCoefficient = 1
+    sqrtDist = 0
+    simpleRootInt = 0
+    exp = dist**2
 
-exp = dist**2
+    sqrtDist = '√' + str(round(exp))
+    root = round(exp)
 
-sqrtDist = '√' + str(round(exp))
-root = exp
-
-for i in range(2, rounded**2 + 1):
-    if (root / i).is_integer():
-        if (math.sqrt(root / i)).is_integer():
-            simpleRoot = i
-            rootCoefficient = rootCoefficient * int(math.sqrt(root / i))
-    for x in range(2, simpleRoot**2 + 1):
+    for i in range(2, rounded**2 + 1):
         if (root / i).is_integer():
-            i = 2
-            break
+            if not i == root and not (root / i) == root:
+                if (math.sqrt(root / i)).is_integer():
+                    simpleRootInt = i
+                    rootCoefficient = rootCoefficient * int(math.sqrt(root / i))
+        for x in range(2, simpleRootInt**2 + 1):
+            if (root / i).is_integer():
+                i = 2
+                break
 
-simpleRoot = str(rootCoefficient) + '√' + str(simpleRoot)
+    simpleRoot = str(rootCoefficient) + '√' + str(simpleRootInt)
 
-if math.sqrt(root).is_integer():
-    response = "The distance is " + str(dist) + " or " + str(sqrtDist)
-else:
-    response = "The distance is " + str(dist) + ", " + str(sqrtDist) + ", or " + str(simpleRoot)
+    if math.sqrt(root).is_integer():
+        response = "The distance is " + str(dist) + " or " + str(sqrtDist)
+    else:
+        response = "The distance is " + str(dist) + ", " + str(sqrtDist) + ", or " + str(simpleRoot)
 
-print(response)
+    print(response)
 
-sys.exit()
+while True:
+    main()
+    print("\n Restart? (y/n)")
+    yn = input().lower()
+    if yn == y:
+        print("Restarting Program")
+    elif yn == n:
+        print("N recieved, ending program.")
+        sys.exit
+    else:
+        print("Y or N not recieved, ending program.")
+        sys.exit
