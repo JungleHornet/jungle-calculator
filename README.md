e4f4146c003bb14edd80e773fdbee109a4d407ac
BuildTools <connore015@gmail.com>
2024-02-25 20:51:37 -0600

    added license

--- README.md
+++ README.md
@@ -21,3 +21,23 @@ Note About Git Repo: dev branch is for development and once a release is complet
 
 Features to add :
 - ?
+
+---
+
+
+JungleCalculator
+Copyright (C) 2024  JungleHornet
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
--- distance_calc.go
+++ distance_calc.go
@@ -1,14 +1,32 @@
+/*
+JungleCalculator - An open-source Go calculator for advanced math functions.
+Copyright (C) 2024  JungleHornet
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package main
 
 import (
 	"fmt"
+	"github.com/junglehornet/goScan"
+	"github.com/junglehornet/junglemath"
 	"log"
 	"math"
 	"regexp"
 	"strconv"
 	"strings"
-	"github.com/junglehornet/goScan"
-	"github.com/junglehornet/junglemath"
 )
 
 func distanceCalc() bool {
