b85f72faf946d71a7b8201654fdb32ecf34381fe
JungleHornet <connore015@gmail.com>
2024-03-02 11:52:53 -0600

    finished -help (for now)

--- cmd/jcalc/main.go
+++ cmd/jcalc/main.go
@@ -141,7 +141,7 @@ func main() {
 					}
 				}
 			}
-		case "new":
+		case "-new":
 			if argLen > 2 {
 				varType := args[2]
 				if argLen > 3 {
@@ -217,9 +217,12 @@ func main() {
 			fmt.Println("Commands:")
 			fmt.Println("WARNING. THIS IS A DEV BUILD. SOME COMMANDS MAY NOT BE IMPLEMENTED.")
 			fmt.Println("    \033[1;32mjcalc -f [function] [args] - Use standalone functions \033[0m\n        pythag [leg 1 length] [leg 2 length] - Pythagorean Theorem Calculator \n        calc - Open general the Calculator")
-			fmt.Println("    \033[1;32mjcalc new [type] [name] [values] - Create a new variable \033[0m\n        point [x] [y] [z (optional, default 0)] - Create a new point \n        line [point1] [point2] - Create a new line \n        triangle [point1] [point2] [point3] - Create a new triangle \n        angle [point1] [point2] [point3] - Create a new angle")
+			fmt.Println("    \033[1;32mjcalc -new [type] [name] [values] - Create a new variable \033[0m\n        point [x] [y] [z (optional, default 0)] - Create a new point \n        line [point1] [point2] - Create a new line \n        triangle [point1] [point2] [point3] - Create a new triangle \n        angle [point1] [point2] [point3] - Create a new angle")
 			fmt.Println("    \033[1;32mjcalc -vars [command] [args] - View/modify stored variables \033[0m\n        [no command] - View all variables and their values \n        clear - Clear all variables \n        delete [variable] - Delete a variable")
-			fmt.Println("    \033[1;32mjcalc [variable] [function] - Do operations on a variable \033[0m\n        [no function] - View a variable and it's values \n        set [values] - Set a variable's values \n        delete - Delete a variable \n        ")
+			fmt.Println("    \033[1;32mjcalc [variable] [function] - Do operations on a variable \033[0m\n        [no function] - View a variable and it's values \n        set [values] - Set a variable's values \n        delete - Delete a variable \n        \033[1;32mVariable type specific:\033[0m")
+			fmt.Println("                \u001B[1;32mLine:\u001B[0m \n                len/length - Measure the length of the line")
+			fmt.Println("                \u001B[1;32mAngle:\u001B[0m \n                measure - Get the measure of the angle")
+			fmt.Println("                \u001B[1;32mTriangle:\u001B[0m \n                orthocenter - Get the orthocenter of the triangle \n                circumcenter - Get the circumcenter of the triangle \n                centroid - Get the centroid of the triangle \n                incenter - Get the incenter of the triangle \n                orthocenter - Get the orthocenter of the triangle \n                parts - Get the info on each angle and side of the triangle.")
 			fmt.Println("    \033[1;32mjcalc -help - Usage help")
 			return
 		}
